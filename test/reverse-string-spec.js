5926423462b99d9b7c44a223c9d1a8379f1d13ae
h-guertler <guertler99@gmail.com>
2023-05-25 08:49:24 -0400

    end of class

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,12 @@
 // Your code here
+// const chai = require('chai');
+const { expect } = chai.expect;
+
+const reverseString = require("../problems/reverse-string.js");
+
+describe("reverseString()", function() {
+    it("should reverse the input string"), () => {
+        const reversedString = reverseString("fun");
+        expect(reversedString).to.equal("nuf");
+    }
+});
