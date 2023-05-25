4de57982e6dadf4e94076acd4cefbb64ebcccfe4
Rasheed Lindsey <rasheedlindsey@live.com>
2023-05-25 18:49:56 -0400

    Phase 1 completed

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,27 @@
-module.exports = function reverseString(string) {
-  // Your code here
-};
+function reverseString(string) {
+  const revArr = [];
+  const strArr = string.split("");
+  strArr.forEach((el) => {
+    revArr.unshift(el);
+  });
+
+  return revArr.join("");
+}
+
+//! Reg for loop solution
+// function reverseString(string) {
+//   const revArr = []
+//   const strArr = string.split("")
+
+//   for (let i = strArr.length-1; i >=0; i--) {
+//     const el = strArr[i]
+//     strArr.pop(el)
+//     strArr.push(el)
+//   }
+
+//   return revArr.join("")
+// };
+console.log(reverseString("fun")); //"nuf"
+console.log(reverseString("gamecube")); //"ebucemag"
+
+module.exports = reverseString;
